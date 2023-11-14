3ed88c4d490a8ee19753e35672c268e2b5320554
Rodolfo Cugler <rodolfocugler@outlook.com>
2023-11-14 20:53:47 +0100

    Update dockerfile

--- iDrink/src/services/cocktail-machine-api.js
+++ iDrink/src/services/cocktail-machine-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const cocktailMachineApi = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'http://rasp-pi:5000/api',
     headers: {'Content-Type': 'application/json'}
 });
 
